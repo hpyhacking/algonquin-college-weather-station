15beb7b5d975228abb2699ab6d8dccda74c4da8c
Liang Qiu <github@hpyhacking.me>
2022-12-01 06:33:43 -0500

    separate css file

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,29 +1,34 @@
 const path = require('path');
 const webpack = require("webpack")
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'app.js',
     path: path.resolve(__dirname, '..', 'dist'),
+    filename: 'app.js',
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "app.css"
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.(scss)$/,
-        use: ['style-loader','css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },
   devServer: {
     static: './dist'
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    })
-  ]
+  }
 };
 
 
