813a7b3f3b716baccd5709cae72efc47df8665e1
Liang Qiu <github@hpyhacking.me>
2022-12-06 15:57:38 -0500

    add weather-card files

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,9 +1,10 @@
-const loadCurrentCard = require('../ui/current_card')
+const WeatherCard = require('../ui/weather_card')
 
 $(document).ready(function() {
   if ($("meta[name=page]").attr('content') != "index") {
     return 
   }
 
-  loadCurrentCard()
+  // TODO: load all data-city elements, 
+  // and use each single one to init WeatherCard and render it.
 })
---
+++ src/app/ui/weather_card.js
@@ -0,0 +1,14 @@
+const Weather = require('../modules/weather')
+
+class WeatherCard() {
+  constrctor(element) {
+    // get data-lat, data-lon and data-city from element attributes
+  }
+
+  render() {
+    // use Weather load data to append info to this element
+  }
+}
+
+module.exports = WeatherCard
+
