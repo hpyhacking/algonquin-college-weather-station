7c66d48bb69b98332901439707ad48664a2511c0
Pengyong Zhan <zpyseagull@163.com>
2022-12-06 23:05:49 -0500

    added weather card functions
    1. added card related tags in index.html
    2. edited the class WeatherCard in weather_card.js
    3. get card list and apply the method from WeatherCard

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -6,5 +6,12 @@ $(document).ready(function() {
   }
 
   // TODO: load all data-city elements, 
+  let cityList = $("[data-city]");
+
   // and use each single one to init WeatherCard and render it.
+  for (let city of cityList) {
+    console.log(city.getAttribute("data-lat"));
+    let card = new WeatherCard(city.getAttribute("data-lat"), city.getAttribute("data-lon"), city.getAttribute("data-city"));
+    card.renderCard(city.getAttribute("data-lat"), city.getAttribute("data-lon"), city.getAttribute("data-city"));
+  }
 })
--- src/app/ui/weather_card.js
+++ src/app/ui/weather_card.js
@@ -1,12 +1,23 @@
 const Weather = require('../modules/weather')
 
 class WeatherCard {
-  constrctor(element) {
+  constrctor(dataLat, dataLon, dataCity) {
     // get data-lat, data-lon and data-city from element attributes
+    this.dataLat = dataLat;
+    this.dataLon = dataLon;
+    this.datacity = dataCity;
   }
 
-  render() {
+  renderCard(dataLat, dataLon, dataCity) {
     // use Weather load data to append info to this element
+    let cardGeoForWeather = new Weather(dataLat, dataLon);
+ 
+    cardGeoForWeather.request(function(data){
+    $("[data-city=" + "'" + dataCity + "'" + "] " + "#current-temp").text(data.current.temp + "°");
+    $("[data-city=" + "'" + dataCity + "'" + "] " + "#weather-desc").text(data.current.weather[0].description);
+    $("[data-city=" + "'" + dataCity + "'" + "] " + "#temp-min").text("H: " + data.daily[0].temp.min + "°");
+    $("[data-city=" + "'" + dataCity + "'" + "] " + "#temp-max").text("L: " + data.daily[0].temp.max + "°");
+    })
   }
 }
 
