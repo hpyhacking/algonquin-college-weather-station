b01544ca63e91bd49d9476e9aae2034973b34935
ShuihanJiang <jian0128@algonquinlive.com>
2022-12-07 02:38:59 -0500

    update contact about html contact js

--- src/app/pages/contact_us.js
+++ src/app/pages/contact_us.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
       //display warning if name is not entered
       if (profileForm.name.value === "") {
           document.querySelector('#nameWarning').textContent = "*Please enter your name*";
-          document.querySelector('#name').style.borderColor = '#ff0000'
           valid = false;
       }
 
@@ -19,20 +18,17 @@ $(document).ready(function() {
       if (profileForm.pronoun.options.selectedIndex === 0) {
           valid = false;
           document.querySelector('#pronounWarning').textContent = "*Please select your pronoun*";
-          document.querySelector('#pronoun').style.borderColor = '#ff0000'
       }
 
       //display warning if phone is not entered
-      if (profileForm.phone.value == "") {
+      if (profileForm.phone.value === "") {
           document.querySelector('#phoneWarning').textContent = "*Please enter your phone number*";
-          document.querySelector('#phone').style.borderColor = '#ff0000'
           valid = false;
       }
 
       //display warning if email is not entered
-      if (profileForm.email.value == "") {
+      if (profileForm.email.value === "") {
           document.querySelector('#emailWarning').textContent = "*Please enter your email*";
-          document.querySelector('#email').style.borderColor = '#ff0000'
           valid = false;
       }
 
@@ -51,13 +47,10 @@ $(document).ready(function() {
           }
 
       //display warning if information is not entered
-      if (document.querySelector("#describe").style.display == "block") {
-          if (profileForm.describe.value == "") {
-              document.querySelector('#describeWarning').textContent = "*Please describe information*";
-              document.querySelector('#describe').style.borderColor = '#ff0000'
-              valid = false;
+      if (profileForm.describe.value == "") {
+            document.querySelector('#describeWarning').textContent = "*Please describe information*";
+            valid = false;
           }
-      }
 
       //validate number    
       const Phone = document.querySelector('#phone')
@@ -82,16 +75,18 @@ $(document).ready(function() {
       // valid than submit
       if (valid) {
           alert("Submit successfully!");
-      }
-      return valid;
-  }
+        window.location.href="http://127.0.0.1:8080/contact_us.html";
+      } 
+
+    }
+
+
 
   /*                                                     events                                                        */
   //remove warning if name has input
   document.querySelector('#name').addEventListener("blur", function () {
       if (this.value !== "") {
           nameWarning.textContent = "";
-          name.style.borderColor = ''
       }
   });
 
@@ -99,15 +94,13 @@ $(document).ready(function() {
   document.querySelector('#pronoun').addEventListener("blur", function () {
       if (profileForm.pronoun.options.selectedIndex !== 0) {
           pronounWarning.textContent = "";
-          pronoun.style.borderColor = ''
       }
   });
 
   //remove warning if phone has input
-  document.querySelector('#contactPhone').addEventListener("blur", function () {
+  document.querySelector('#phone').addEventListener("blur", function () {
       if (this.value !== "")  {
-          contactPhoneWarning.textContent = "";
-          contactPhone.style.borderColor = ''
+          phoneWarning.textContent = "";
       }
   });
 
@@ -115,7 +108,6 @@ $(document).ready(function() {
   document.querySelector('#email').addEventListener("blur", function () {
       if (this.value !== "")  {
           emailWarning.textContent = "";
-          email.style.borderColor = ''
       }
   });
 
@@ -145,18 +137,17 @@ $(document).ready(function() {
   document.querySelector('#describe').addEventListener("blur", function () {
       if (this.value !== "") {
           describeWarning.textContent = "";
-          describe.style.borderColor = ''
       }
   });
 
-  // change warning if input is not number
+  // change warning if phone is not number
   document.querySelector('#phone').addEventListener("blur", function () {
       if ((!this.value.match(/^\d+/)) &&(this.value.length === 10)) {
           document.querySelector('#phoneWarning').textContent = '*Phone should be numbers*';
       }
   })
 
-  // change warning if input is not 10 digits
+  // change warning if phone is not 10 digits
   document.querySelector('#phone').addEventListener("blur", function () {
       if ((this.value.match(/^\d+/)) &&(this.value.length !== 10)) {
           document.querySelector('#phoneWarning').textContent = '*Phone should be 10 digits*';
@@ -165,4 +156,30 @@ $(document).ready(function() {
 
   // Add an event to the form on submit to validate input
   document.profileForm.addEventListener("submit", validate);
+
+  // Add an event to the form on reset to clear input and warning
+  function reset() {
+    document.querySelector('#nameWarning').textContent = "";
+    document.querySelector('#pronounWarning').textContent = "";
+    document.querySelector('#phoneWarning').textContent = "";
+    document.querySelector('#emailWarning').textContent = "";
+    document.querySelector('#departmentWarning').textContent = "";
+    document.querySelector('#describeWarning').textContent = "";
+  }
+
+  $("#reset").click(function(){  
+    $(".alert alert-danger").text("");
+  })
+
+
+
+  
 })
+
+
+
+
+
+
+
+
