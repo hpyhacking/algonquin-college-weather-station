c978ed7c3a1bcaa70780bdb6eff284d072e54562
Liang Qiu <github@hpyhacking.me>
2022-12-07 01:04:42 -0500

    styles for search input, and make behavior common sense

--- src/app/ui/search.js
+++ src/app/ui/search.js
@@ -2,35 +2,55 @@ const Geocoding = require('../modules/geocoding')
 
 const ITEM_TEMPLATE = `
 <li data-lat='#lat#' data-lon='#lon#' data-city='#city#' data-country='#country#'>
-  #city#, #country#
+  <i class="fas fa-search"></i>
+  #city##country#
 </li>
 `
 function applySearching(element) {
+  $(window).on('click', function() {
+    $(element).find("ul").hide()
+  })
+
+  $(element).find("input[type=search]").click(function(event) {
+    event.stopPropagation()
+  })
+
+  let width = $(element).find("input[type=search]").outerWidth()
+  $(element).find("ul").css("width", width)
+
   $(element).find('input[type=search]').on('input', function(){
     clearTimeout(this.delay);
     this.delay = setTimeout(function(){
       $(this).trigger('search')
-    }.bind(this), 800)
+    }.bind(this), 300)
+  }).on('focus', function() {
+    if(this.value){
+      $(element).find("ul").show()
+    }
   }).on('search', function(){
     if(this.value){
-
       Geocoding.query(this.value, function(data) {
         let results = $(element).find('ul').empty()
 
         data.forEach(function(result) {
+          $(element).find("ul").show()
           let r = ITEM_TEMPLATE.replaceAll("#lat#", result.lat)
                                .replaceAll("#lon#", result.lon)
                                .replaceAll("#city#", result.city)
 
-          if (result.state) {
-            r = r.replaceAll("#country#", `${result.state}, ${result.country_code}`)
+          if (result.state && result.country_code) {
+            r = r.replaceAll("#country#", `, ${result.state}, ${result.country_code}`)
+          } else if (result.country_code) {
+            r = r.replaceAll("#country#", `, ${result.country_code}`)
           } else {
-            r = r.replaceAll("#country#", result.country)
+            r = r.replaceAll("#country#", "")
           }
 
           results.append($(r))
         })
       })
+    } else {
+      $(element).find("ul").hide()
     }
   })
 
