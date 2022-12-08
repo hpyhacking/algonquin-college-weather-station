64266434bb8110107aa04f230fd550fa7a561875
Liang Qiu <github@hpyhacking.me>
2022-12-08 17:45:44 -0500

    fixed relative path for weather.html

--- src/app/ui/search.js
+++ src/app/ui/search.js
@@ -82,7 +82,7 @@ function applySearching(element) {
     let state = encodeURIComponent($(this).data('state'))
     let country = encodeURIComponent($(this).data('country'))
 
-    window.location.href = `/weather.html?lat=${lat}&lon=${lon}&city=${city}&country=${country}`
+    window.location.href = `./weather.html?lat=${lat}&lon=${lon}&city=${city}&country=${country}`
     $(element).find('input[type=search]').val("")
   })
 }
