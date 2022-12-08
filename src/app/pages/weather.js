0129794dc66d994d73e35d1f7794bc8ab059ab76
Pengyong-Zhan <113105360+Pengyong-Zhan@users.noreply.github.com>
2022-12-08 18:25:26 -0500

    added daily forecast and icon using i tags  (#24)
    * added icon and daily forecast function
    
    * modified div to i tags
    
    * updated i tag

--- src/app/pages/weather.js
+++ src/app/pages/weather.js
@@ -1,49 +1,93 @@
-const Weather = require('../modules/weather.js');
+const Weather = require('../modules/weather.js')
 
 $(document).ready(function() {
     if ($("meta[name=page]").attr('content') != "weather") {
-      return;
+      return
     }
   
-    let urlParams = new URLSearchParams(window.location.search);
-    let lat = urlParams.get('lat');
-    let lon = urlParams.get('lon');
-    let cityName = urlParams.get('city');
-    let geoForWeather = new Weather(lat, lon);
+    let urlParams = new URLSearchParams(window.location.search)
+    let lat = urlParams.get('lat')
+    let lon = urlParams.get('lon')
+    let cityName = urlParams.get('city')
+    let geoForWeather = new Weather(lat, lon)
 
     geoForWeather.request(function(data){
-        $("#cityName").text(cityName);
-        $("#currentTemp").text(data.current.temp + "°");
-        $("#weatherDesc").text(data.current.weather[0].description);
-        $("#tempMin").text("H: " + data.daily[0].temp.min + "°");
-        $("#tempMax").text("L: " + data.daily[0].temp.max + "°");
+        //overall weather
+        $("#cityName").text(cityName)
+        $("#currentTemp").text(data.current.temp + "°")
+        $("#weatherDesc").text(data.current.weather[0].description)
+        $("#tempMin").text("H: " + data.daily[0].temp.min + "°")
+        $("#tempMax").text("L: " + data.daily[0].temp.max + "°")
 
-        //get and display hourly data
-        let hourly24 = data.hourly.slice(0, 24);
+        //24 hourly weather
+        let hourly24 = data.hourly.slice(0, 24)
 
-        for (let hourly of hourly24) {
-          var timeStamp= hourly.dt;
-          var dateFormat = new Date(timeStamp*1000);
-          let getHour = dateFormat.getHours().toString();
+        $(".hourly").each(function(i, element){
+          var timeStamp= hourly24[i].dt
+          var dateFormat = new Date(timeStamp*1000)
+          let getHour = dateFormat.getHours().toString()
 
-          let formmatedHour;
+          let formmatedHour
           if (getHour.length === 1) {
-            formmatedHour = '0'+ getHour;
+            formmatedHour = '0'+ getHour
           } else {
-            formmatedHour = getHour;
+            formmatedHour = getHour
           }
+          
+          $(element).find(".formmatedHour").text(formmatedHour)
+          $(element).find(".hourlyTemp").text(hourly24[i].temp + "°")
+          $(element).find("i").addClass(getIcon(hourly24[i].weather[0].main))
+        })
 
-          let i = hourly24.indexOf(hourly);
-          $(".formmatedHour")[i].textContent = formmatedHour;
-          $(".hourlyWeatherDesc")[i].textContent = hourly.weather[0].description;
-          $(".hourlyTemp")[i].textContent = hourly.temp + "°";          
-        }
+        //7 daily weather
+        let daily7 = data.daily.slice(1, 8)
+        $(".daily").each(function(i, element){
+          var timeStamp= daily7[i].dt
+          var dateFormat = new Date(timeStamp*1000)
+          let dayOfWeek = dateFormat.toDateString().slice(0, 4)
+          $(element).find(".dayOfWeek").text(dayOfWeek)
+          $(element).find("i").addClass(getIcon(daily7[i].weather[0].main))
+          $(element).find(".dailyTempMin").text(daily7[i].temp.min + "°")
+          $(element).find(".dailyTempMax").text(daily7[i].temp.max + "°")
+        })
 
         //other weather details
-        $("#liFeelsLike").text(data.current["feels_like"] + "°");
-        $("#liVisibility").text(data.current.visibility + " km");
-        $("#liHumidity").text(data.current.humidity + "%");
-        $("#liWindSpeed").text(data.current["wind_speed"] + " km/h");
+        $("#feelsLike").text(data.current["feels_like"] + "°")
+        $("#visibility").text(data.current.visibility + " km")
+        $("#humidity").text(data.current.humidity + "%")
+        $("#windSpeed").text(data.current["wind_speed"] + " km/h")
     });
+
+
+    function getIcon(descMain) {
+      let icon;
+      if (descMain === "Thunderstorm") {
+        icon = "fas fa-poo-storm"
+      }
+      else if (descMain === "Drizzle") {
+        icon = "fas fa-cloud-rain"
+      }
+      else if (descMain === "Rain") {
+        icon = "fas fa-cloud-showers-heavy"
+      }
+      else if (descMain === "Snow") {
+        icon = "fas fa-snowflake"
+      }
+      else if (descMain === 'Clouds') {
+        icon = "fas fa-cloud"
+      }
+      else if (descMain === 'Clear') {
+        icon = "fas fa-sun"
+      }
+      else if (descMain === "Mist")  {
+        icon = "fas fa-smog"
+      }
+      else if (descMain === "Fog")  {
+        icon = "fas fa-smog"
+      }
+
+      return icon
+    } 
+
   })
   
