43f4f5d0457940652c556d3cfee0ab230a914cc3
Liang Qiu <github@hpyhacking.me>
2022-12-08 17:28:47 -0500

    add webpack publish config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,28 @@
 const { merge } = require('webpack-merge')
 const base = require('./_webpack.js')
+const path = require('path')
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(base, {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '..', 'docs'),
+    filename: 'app.js',
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "*.html",
+          to: path.resolve(__dirname, '..', 'docs'),
+          context: path.resolve(__dirname, '..', 'dist'),
+        },
+        {
+          from: "favicon*",
+          to: path.resolve(__dirname, '..', 'docs'),
+          context: path.resolve(__dirname, '..', 'dist'),
+        }
+      ],
+    })
+  ]
 });
