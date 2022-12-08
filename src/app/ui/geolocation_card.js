04e9722a6873e672823032686850e905ed01cdd8
Liang Qiu <github@hpyhacking.me>
2022-12-08 09:34:20 -0500

    save geolocation lat & lon into sessionStorage

--- src/app/ui/geolocation_card.js
+++ src/app/ui/geolocation_card.js
@@ -3,33 +3,49 @@ const Geocoding = require('../modules/geocoding')
 
 class GeolocationCard {
   static render(element) {
+    let render_card = function(lat, lon, city) {
+      $(element).data('city', city)
+      $(element).data('lat', lat)
+      $(element).data('lon', lon)
+
+      let card = new WeatherCard($(element))
+      card.render()
+    }
+
+    // check sessionStorage previous geolocation info
+    let session_lat = window.sessionStorage.getItem("lat");
+    let session_lon = window.sessionStorage.getItem("lon");
+    let session_city = window.sessionStorage.getItem("city");
+
+    if (session_lat && session_lon && session_city) {
+      render_card(session_lat, session_lon, session_city)
+      return
+    }
+  
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(data) {
-
         let lat = data.coords.latitude.toFixed(2)
         let lon = data.coords.longitude.toFixed(2)
+
+        window.sessionStorage.setItem('lat', lat)
+        window.sessionStorage.setItem('lon', lon)
+
         let key = `city[${lat},${lon}]`
-
-        let render_card = function(city) {
-          $(element).data('city', city)
-          $(element).data('lat', lat)
-          $(element).data('lon', lon)
-
-          let card = new WeatherCard($(element))
-          card.render()
-        }
 
         let cached_city = window.localStorage.getItem(key)
 
         if (cached_city) {
-          render_card(cached_city)
+          window.sessionStorage.setItem('city', cached_city)
+          render_card(lat, lon, cached_city)
         } else {
           Geocoding.query(`${lat},${lon}`, function(data) {
             let result = data[0]
 
             if (result) {
               window.localStorage.setItem(key, result.city)
-              render_card(result.city)
+              window.sessionStorage.setItem('city', result.city)
+
+              render_card(lat, lon, result.city)
             }
           })
         }
