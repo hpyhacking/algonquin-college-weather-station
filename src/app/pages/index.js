7df6a5d89bf82f00d133246bf221dbe7c62ab259
Liang Qiu <github@hpyhacking.me>
2022-12-08 15:30:09 -0500

    from watching list loading cities

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,5 +1,5 @@
 const GeolocationCard = require('../ui/geolocation_card')
-const WeatherCard = require('../ui/weather_card')
+const WatchingList = require('../ui/watching_list')
 const applySearching = require('../ui/search')
 
 $(document).ready(function() {
@@ -7,15 +7,14 @@ $(document).ready(function() {
     return 
   }
 
+  let watching_list = new WatchingList()
+  watching_list.render($('#watching_list'))
+
   if (navigator.geolocation) {
     // geolocation card loading when browser support
     GeolocationCard.render($("#geolocation-card"))
   }
   
   applySearching($("#searching"))
-
-  $("[data-city]").each(function(_, element){
-    new WeatherCard(element).render()
-  })
 })
 
---
+++ src/app/ui/watching_list.js
@@ -0,0 +1,53 @@
+const WeatherCard = require('../ui/weather_card')
+
+const DEFAULT_LIST = [
+  {city: 'Ottawa', lat: '45.4209', lon: '-75.6901'},
+  {city: 'Pembroke', lat: '45.8261', lon: '-77.1135'},
+  {city: 'Perth', lat: '44.9027', lon: '-76.2461'}
+]
+
+class WatchingList {
+  #list
+  #name
+
+  get key() {
+    return `watching-list-${this.#name}`
+  }
+
+  constructor(_name) {
+    this.#name = _name ?? 'default'
+    this.#list = JSON.parse(window.localStorage.getItem(this.key)) ?? DEFAULT_LIST
+    window.localStorage.setItem(this.key, JSON.stringify(this.#list))
+  }
+
+  add(city, lat, lon) {
+    let list = JSON.parse(window.localStorage.getItem(this.key))
+    list.push({city: city, lat: lat, lon: lon})
+    window.localStorage.setItem(this.key, JSON.stringify(list))
+  }
+
+  remove(lat, lon) {
+    let list = JSON.parse(window.localStorage.getItem(this.key))
+
+    let new_list = $.grep(list, function(i){ 
+      return !(i.lat == lat && i.lon == lon)
+    })
+
+    window.localStorage.setItem(this.key, JSON.stringify(new_list))
+  }
+
+  render(element) {
+    this.#list.forEach(function(item) {
+      let card_wrapper = $("#weather-card-template").clone().removeAttr("id")
+
+      let card = card_wrapper.find(".weather-card")
+      card.data("lat", item.lat).data("lon", item.lon).data("city", item.city)
+      new WeatherCard(card).render()
+
+      card_wrapper.appendTo($(element))
+    })
+  }
+}
+
+module.exports = WatchingList
+
