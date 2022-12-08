ce3e38f76c46a4146843534a3da153d34b3ba791
Pengyong Zhan <zpyseagull@163.com>
2022-12-07 22:56:30 -0500

    added current location weather function
    1. added an id attr in index.html
    2. updated current location card code

--- src/app/ui/geolocation_card.js
+++ src/app/ui/geolocation_card.js
@@ -1,13 +1,37 @@
+const Config = require('../modules/config');
 const Weather = require('../modules/weather')
+const Geocoding = require('../modules/geocoding')
+const ENDPOINT_LOCATION = "http://api.openweathermap.org/geo/1.0/reverse"
+
+const CARD_TEMPLATE = `
+<h3>#city#</h3>
+<p>#temp#</p>
+<p>#description#</p>
+<p><span>L:</span> #min#</p>
+<p><span>H:</span> #max#</p>
+`
 
 class GeolocationCard {
   static render(element) {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(data) {
-        let lat = position.coords.latitude
-        let lon = position.coords.longitude
-        
-        // use Weather load data to append info to this element
+        let lat = data.coords.latitude
+        let lon = data.coords.longitude
+
+        let locationWeather = new Weather(lat, lon)
+        locationWeather.request(function(data){
+          let card = CARD_TEMPLATE.replace('#temp#', data.current.temp)
+                                .replace('#description#', data.current.weather[0].description)
+                                .replace('#min#', data.daily[0].temp.min)
+                                .replace('#max#', data.daily[0].temp.max)
+                                
+          $.getJSON(`${ENDPOINT_LOCATION}?lat=${lat.toFixed(4)}&lon=${lon.toFixed(4)}&limit=1&appid=${Config.API_KEY}`, 
+            function(data){
+            card = card.replace('#city#', data[0].name)
+            console.log(card);
+            $(element).empty().append($(card))
+          })
+        })
       });
     } else { 
       $(element).find('.error').show()
