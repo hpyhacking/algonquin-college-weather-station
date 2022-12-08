1874d35d5a7d451d04ed11c8fa5d5f52d6820a5e
Pengyong Zhan <zpyseagull@163.com>
2022-12-07 22:56:49 -0500

    Update index.js

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,9 +1,10 @@
 const loadCurrentCard = require('../ui/current_card')
+const geoLocationCard = require('../ui/geolocation_card')
 
 $(document).ready(function() {
   if ($("meta[name=page]").attr('content') != "index") {
     return 
   }
 
-  loadCurrentCard()
+  geoLocationCard.render($("#geoLocationCard"))
 })
