fedf74710bc5533364a9fbc680ea60959d7eee6a
ShuihanJiang <113075913+ShuihanJiang@users.noreply.github.com>
2022-12-09 17:37:56 -0500

    update contact and about pages (#31)

--- src/app/pages/contact_us.js
+++ src/app/pages/contact_us.js
@@ -9,35 +9,26 @@ $(document).ready(function() {
       var valid = true;
 
       //display warning if name is not entered
-      if (profileForm.name.value === "") {
-          document.querySelector('#nameWarning').textContent = "*Please enter your name*";
-          document.querySelector('#name').style.borderColor = '#ff0000'
+      if (profile.name.value.trim() === "") {
+        $("#nameWarning").text ("*Please enter your name*");
           valid = false;
       }
 
       // // display warning if pronoun is not selected
-      if (profileForm.pronoun.options.selectedIndex === 0) {
+      if (profile.pronoun.options.selectedIndex === 0) {
           valid = false;
-          document.querySelector('#pronounWarning').textContent = "*Please select your pronoun*";
-          document.querySelector('#pronoun').style.borderColor = '#ff0000'
+          $("#pronounWarning").text ( "*Please select your pronoun*");
       }
 
-      //display warning if phone is not entered
-      if (profileForm.phone.value == "") {
-          document.querySelector('#phoneWarning').textContent = "*Please enter your phone number*";
-          document.querySelector('#phone').style.borderColor = '#ff0000'
-          valid = false;
-      }
-
-      //display warning if email is not entered
-      if (profileForm.email.value == "") {
-          document.querySelector('#emailWarning').textContent = "*Please enter your email*";
-          document.querySelector('#email').style.borderColor = '#ff0000'
+      //display warning if phone is not vaild
+      const phone_regex = /^\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}$/
+      if (phone_regex.test(phone) == false)  {
+          $('#phoneWarning').text ( "*This is not a vaild phone*");
           valid = false;
       }
 
       //display warning if department is not checked
-      const radios = document.querySelectorAll('[name = "department"]');
+      const radios = $('[name = "department"]');
           var checked = false
           for (var i = 0, l = radios.length; i < l; i++) {
               if (radios[i].checked) {
@@ -47,122 +38,109 @@ $(document).ready(function() {
           }
           if (checked == false) {
               valid = false;
-              document.querySelector('#departmentWarning').textContent = "*Department is required*";
+              $('#departmentWarning').text ("*Department is required*");
           }
 
       //display warning if information is not entered
-      if (document.querySelector("#describe").style.display == "block") {
-          if (profileForm.describe.value == "") {
-              document.querySelector('#describeWarning').textContent = "*Please describe information*";
-              document.querySelector('#describe').style.borderColor = '#ff0000'
-              valid = false;
+      if (profile.describe.value.trim() == "") {
+        $('#describeWarning').text("Please describe information*");
+            valid = false;
           }
-      }
-
-      //validate number    
-      const Phone = document.querySelector('#phone')
-      if ((!Phone.value.match(/^\d+/))&&(profileForm.phone.value !== "")) {
-          valid = false;
-          document.querySelector('#phoneWarning').textContent = "*Phone should be numbers*";
-      }
-
-      // validate 10 digits
-      if ((Phone.value.length !== 10)&&(profileForm.phone.value !== "")) {
-          valid = false;
-          document.querySelector('#phoneWarning').textContent = "*Phone should be 10 digits*";
-      }
 
       //validate email
-      const Email = document.querySelector('#email').value; 
-      if (Email.indexOf("@") < 1 || Email.lastIndexOf(".") < Email.indexOf("@") + 2 || Email.lastIndexOf(".")+2 >= Email.length) { 
-          valid = false;
-          document.querySelector('#emailWarning').textContent = "*This is not a vaild email*";
+      const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+      if (EMAIL_REGEX.test(email) == false) {
+        valid = false;
+        $('#emailWarning').text ("*This is not a vaild email*");
       }
-
+   
       // valid than submit
       if (valid) {
           alert("Submit successfully!");
-      }
-      return valid;
-  }
+        window.location.href="https://hpyhacking.github.io/algonquin-college-weather-station/contact_us.html";
+      } 
+
+    }
+
+
 
   /*                                                     events                                                        */
   //remove warning if name has input
-  document.querySelector('#name').addEventListener("blur", function () {
+  $('#name').blur(function () {
       if (this.value !== "") {
-          nameWarning.textContent = "";
-          name.style.borderColor = ''
+        $("#nameWarning").text ("") ;
       }
   });
 
   //*remove warning if pronoun has selected
-  document.querySelector('#pronoun').addEventListener("blur", function () {
-      if (profileForm.pronoun.options.selectedIndex !== 0) {
-          pronounWarning.textContent = "";
-          pronoun.style.borderColor = ''
+  $('#pronoun').blur(function () {
+      if (profile.pronoun.options.selectedIndex !== 0) {
+        $("#pronounWarning").text ("");
       }
   });
 
-  //remove warning if phone has input
-  document.querySelector('#contactPhone').addEventListener("blur", function () {
-      if (this.value !== "")  {
-          contactPhoneWarning.textContent = "";
-          contactPhone.style.borderColor = ''
+  //remove warning if phone is vaild
+  const phone_regex = /^\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}$/
+  $('#phone').blur(function() {
+      if (phone_regex.test(phone) == true)  {
+        $('#phoneWarning').text ("");
       }
   });
 
-  //remove warning if email has input
-  document.querySelector('#email').addEventListener("blur", function () {
-      if (this.value !== "")  {
-          emailWarning.textContent = "";
-          email.style.borderColor = ''
+  //remove warning if email is vaild
+  const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  $('#email').blur(function () {
+      if (EMAIL_REGEX.test(email) == true)  {
+        $('#emailWarning').text ("");
       }
   });
 
   //remove warning if department selected has input
-  document.querySelectorAll('[name = "department"]')[0].addEventListener("blur", function () {
+  $('[name = "department"]')[0].blur(function () {
       if (this.checked) {
-          document.querySelector('#departmentWarning').textContent = '';
+        $('#departmentWarning').text ("");
       }
   });
-  document.querySelectorAll('[name = "department"]')[1].addEventListener("blur", function () {
+  $('[name = "department"]')[1].blur(function () {
       if (this.checked) {
-          document.querySelector('#departmentWarning').textContent = '';
+        $('#departmentWarning').text ("");
       }
   });
-  document.querySelectorAll('[name = "department"]')[2].addEventListener("blur", function () {
+  $('[name = "department"]')[2].blur(function () {
       if (this.checked) {
-          document.querySelector('#departmentWarning').textContent = '';
+        $('#departmentWarning').text ("");
       }
   });
-  document.querySelectorAll('[name = "department"]')[3].addEventListener("blur", function () {
+  $('[name = "department"]')[3].blur(function () {
       if (this.checked) {
-          document.querySelector('#departmentWarning').textContent = '';
+        $('#departmentWarning').text ("");
       }
   });
 
   //remove warning if describe information has input
-  document.querySelector('#describe').addEventListener("blur", function () {
-      if (this.value !== "") {
-          describeWarning.textContent = "";
-          describe.style.borderColor = ''
+  $('#describe').blur(function () {
+      if (this.value.trim() !== "") {
+        $('#describeWarning').text ("");
       }
   });
 
-  // change warning if input is not number
-  document.querySelector('#phone').addEventListener("blur", function () {
-      if ((!this.value.match(/^\d+/)) &&(this.value.length === 10)) {
-          document.querySelector('#phoneWarning').textContent = '*Phone should be numbers*';
-      }
-  })
-
-  // change warning if input is not 10 digits
-  document.querySelector('#phone').addEventListener("blur", function () {
-      if ((this.value.match(/^\d+/)) &&(this.value.length !== 10)) {
-          document.querySelector('#phoneWarning').textContent = '*Phone should be 10 digits*';
-      }
-  })
-
   // Add an event to the form on submit to validate input
-  document.profileForm.addEventListener("submit", validate);
+  document.profile.addEventListener("submit", validate);
+  
+  // Add an event to the form on reset to clear warning
+  $("#reset").click(function(){  
+    $(".alert").text("");
+  })
+
+
+
+  
 })
+
+
+
+
+
+
+
+
